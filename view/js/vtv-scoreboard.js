da500cba9acfd00b88b6f630981cfa2a2970ebb3
Matthew McNamara <matt@mattmcn.com>
2014-08-16 20:10:17 +0100

    Fixed team colours not swapping, admin panel finished

--- view/js/vtv-scoreboard.js
+++ view/js/vtv-scoreboard.js
@@ -1,16 +1,50 @@
 $(document).ready(function() {
-    //nodecg.listenFor('update', update);
+  nodecg.listenFor('update', update);
 
-    var map = [];
-    map[0] = $('#map1');
-    map[1] = $('#map2');
-    map[2] = $('#map3');
-    map[3] = $('#map4');
-    map[4] = $('#map5');
+  var maps = [];
+  maps[0] = $('#map1');
+  maps[1] = $('#map2');
+  maps[2] = $('#map3');
+  maps[3] = $('#map4');
+  maps[4] = $('#map5');
 
-    var container = $('#container');
+  var container = $('#container');
 
-    // Browser source fix
-    map[3].removeClass('hidden');
-    setTimeout(function() { map[3].addClass('hidden');}, 50);
+  function update(data) {
+    $('.selected').removeClass('selected');
+    maps[data.selected].addClass('selected');
+
+    $('.team.blue').each(function () {
+      $(this).text(data.teamA);
+    });
+    $('.team.red').each(function() {
+      $(this).text(data.teamB);
+    });
+
+    if (data.swapped) {
+      container.addClass('swapped');
+    } else {
+      container.removeClass('swapped');
+    }
+
+    data.maps.forEach(function(map, idx) {
+      if (map.name !== "") {
+        maps[idx].find('.name').text(map.name + ':');
+      } else {
+        maps[idx].find('.name').text('Map ' + (idx+1) + ':');
+      }
+
+      maps[idx].find('.score.blue').text(map.scoreA);
+      maps[idx].find('.score.red').text(map.scoreB);
+      if (map.shown) {
+        maps[idx].removeClass('hidden');
+      } else {
+        maps[idx].addClass('hidden');
+      }
+    });
+  }
+
+  // Browser source fix
+  maps[3].removeClass('hidden');
+  setTimeout(function() { maps[3].addClass('hidden');}, 50);
 });
